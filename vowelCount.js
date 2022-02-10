15e876e00311d83f709127c7a8690c2e287a6a34
frsiam <siam.frs@gmail.com>
2022-02-10 19:26:04 +0600

    create function and check for loop

---
+++ vowelCount.js
@@ -0,0 +1,10 @@
+// vowel check using only if condition
+
+function countVowel(sentence){
+    let count = 0;
+    for(let i = 0; i < sentence.length; i++){
+        console.log(i);
+    }
+    return count;
+}
+countVowel('hello how are you');
